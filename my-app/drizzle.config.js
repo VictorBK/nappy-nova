978b464287b7faacefe46a3c21485cf26db6830a
VictorBK <victorbrian4@gmail.com>
2024-09-30 07:44:45 +0300

    Drizzle configuration

---
+++ my-app/drizzle.config.js
@@ -0,0 +1,10 @@
+export default {
+    dialect: "postgresql",
+    schema: "./src/utils/db/schema.ts",
+    out: "./drizzle",
+    dbCredentials: {
+      url: "postgresql://neondb_owner:xhKSQTlc50sG@ep-holy-band-a5vrs7oy.us-east-2.aws.neon.tech/nappy-nova?sslmode=require",
+      connectionString: "postgresql://neondb_owner:xhKSQTlc50sG@ep-holy-band-a5vrs7oy.us-east-2.aws.neon.tech/nappy-nova?sslmode=require",
+    },
+  };
+  
