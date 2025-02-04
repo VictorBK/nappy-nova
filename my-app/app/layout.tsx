ef887dc826e2fd7cb9d266f1bcb7075a2111612c
VictorBK <victor.kanyangah@gmail.com>
2025-02-04 13:52:24 +0300

    Header projection to localhost handled

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react"
 import { Inter } from 'next/font/google'
 import "./globals.css"
-// import Header from "@/components/Header"
+import Header from "@/components/Header"
 // import Sidebar from "@/components/Sidebar"
 // import 'leaflet/dist/leaflet.css'
 import { Toaster } from 'react-hot-toast'
@@ -25,6 +25,7 @@ children,
       <body className={inter.className}>
         <div className="min-h-screen bg-gray-50 flex flex-col">
           {/* header */}
+          <Header onMenuClick={() => setSidebarOpen(!sidebarOpen)} totalEarnings={totalEarnings}/>
           <div className="flex flex-1">
             {/* sidebar */}
             <main className="flex-1 p-4 lg:p-8 ml-0 lg:ml-64 transition-all duration-300">
